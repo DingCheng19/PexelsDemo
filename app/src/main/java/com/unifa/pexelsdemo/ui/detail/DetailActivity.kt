1497d5fb03632e26966bec3c892db5ae0a23d7a1
DingCheng19 <dingcheng.19891102@gmail.com>
2021-06-12 16:59:23 +0900

    The detail interface element is optimized

--- app/src/main/java/com/unifa/pexelsdemo/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/unifa/pexelsdemo/ui/detail/DetailActivity.kt
@@ -1,18 +1,14 @@
 package com.unifa.pexelsdemo.ui.detail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.unifa.pexelsdemo.R
-import com.unifa.pexelsdemo.data.api.ApiHelper
-import com.unifa.pexelsdemo.data.api.RetrofitBuilder
 import com.unifa.pexelsdemo.databinding.ActivityDetailBinding
-import com.unifa.pexelsdemo.databinding.ActivityMainBinding
-import com.unifa.pexelsdemo.ui.base.ViewModelFactory
-import com.unifa.pexelsdemo.ui.main.MainViewModel
 import com.unifa.pexelsdemo.util.Constant.IMAGEURL
 
 class DetailActivity : AppCompatActivity() {
@@ -21,7 +17,7 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var mActivityDetailBinding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_detail)
+        initActionBar()
         imageUrl = intent.getStringExtra(IMAGEURL).toString()
 
         mActivityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
@@ -29,5 +25,20 @@ class DetailActivity : AppCompatActivity() {
         mActivityDetailBinding.vm = viewModel
         Glide.with(this).load(imageUrl).placeholder(R.drawable.ic_image_placeholder).into(mActivityDetailBinding.image)
 
+    }
+
+    private fun initActionBar() {
+        val actionBar: ActionBar? = this.supportActionBar?.apply {
+            title = "Details"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() === android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
